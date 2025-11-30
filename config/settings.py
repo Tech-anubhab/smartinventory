2fa3832867b26397a4f1669c7c8c1cc1c954a5e1
Anubhab <deyanubhab064@gmail.com>
2025-12-01 02:20:02 +0530

    Add CSRF trusted origins for Railway

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-1488c.up.railway.app",
+]
+
 
 
 # Application definition
