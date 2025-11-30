1f67c34c29105d64688d22cd2e1152dcf2ae12d2
Anubhab <deyanubhab064@gmail.com>
2025-12-01 02:05:14 +0530

    Use env vars for production

--- config/settings.py
+++ config/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!2!fv36b6#v1+ex^@8%7z(%=l8f5xr14g#9^q3nhd!x-i@y&2@'
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
