5d65b9a940ec8d7ff061c97b0a3304ee3b81ee6e
Anubhab <deyanubhab064@gmail.com>
2026-07-05 06:40:57 +0530

    hosting

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -90,10 +90,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=BASE_DIR / 'db.sqlite3',
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
 
 
