59b924b0fe785414ef06ef2c4e8b2ab8121133ee
Anubhab <deyanubhab064@gmail.com>
2026-07-05 07:46:25 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,techanubhab.pythonanywhere.com").split(",")
 CSRF_TRUSTED_ORIGINS = [
     "https://web-production-1488c.up.railway.app",
+    "https://techanubhab.pythonanywhere.com",
 ]
 
 
